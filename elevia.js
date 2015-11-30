592eabe40f7511a73293b3e3e6a6b148c416db2b
Robert Plummer <robertleeplummerjr@gmail.com>
2015-11-30 10:49:39 -0500

    nearly working

--- elevia.js
+++ elevia.js
@@ -1,31 +1,117 @@
-/**
- * @name Elevia
- * @namespace
- */
-Namespace('Elevia').
-Class('Learn', {
-  /**
-   * @param window
-   * @param document
-   * @alias Elevia.Learn
-   */
-  construct: function(window, document) {
-    this.window = window;
-    this.document = document;
+var Elevia = (function() {
+  function Elevia(world) {
+    var self = this;
+    this.waitingUserCountLast = 9999999999999;
+    this.ridingUserCountLast = 0;
+    this.world = world;
+    this.brain = new ann.NeuralNet({
+      inputCount: 15,
+      outputCount: self.world.elevators.length,
+      hiddenLayerCount: 3,
+      sense: function() {
+        return self.flatten();
+      },
+      goal: function() {
+        var reward = 0,
+          waiting = self.waitingUserCount(),
+          riding = self.ridingUserCount();
 
-    this.setupUI();
-  },
-  setupUI: function() {
-    var startstopButton = this.document.querySelector('button.startstop')
-      , learnButton     = this.document.createElement('button')
-      ;
+        if (waiting < self.waitingUserCountLast) {
+          reward++;
 
-    learnButton.setAttribute('style', startstopButton.getAttribute('style'));
-    learnButton.className = startstopButton
-      .className
-      .replace('startstop', '') + ' learn';
-    learnButton.appendChild(this.document.createTextNode('Learn'));
+          self.waitingUserCountLast = waiting;
+        }
 
-    startstopButton.parentNode.insertBefore(learnButton, startstopButton);
+        if (riding < self.ridingUserCountLast) {
+          reward++;
+          self.ridingUserCountLast = riding;
+        }
+
+        return reward;
+      },
+      action: function(controls) {
+        self.world.elevators.forEach(function(elevator, i) {
+          if (controls[i] > 0.01) {
+            var goToFloor = Math.round(controls[i] * self.world.floors.length);
+            console.log('elevator ' + ( i + 1) + ' going to floor ' + goToFloor);
+            elevator.on("idle", function() { elevator.goToFloor(goToFloor + 1); });
+          } else {
+            console.log('no action');
+          }
+        });
+
+      }
+    });
   }
-});
+
+  Elevia.prototype = {
+    ridingUserCount: function() {
+      var count = 0;
+
+      this.world.users.forEach(function(user) {
+        if (user.parent === null) {
+          count++;
+        }
+      });
+
+      return count;
+    },
+    waitingUserCount: function() {
+      var count = 0;
+
+      this.world.users.forEach(function(user) {
+        if (user.parent !== null) {
+          count++;
+        }
+      });
+
+      return count;
+    },
+    flatten: function() {
+      var flat = [],
+        usersInElevators = [],
+        world = this.world,
+        dfi = [], //destination floor importance
+        wfi = [], //waiting floor importance
+        elevators = world.elevators;
+
+      flat.push(world.floors.length);
+
+      world.floors.forEach(function(floor, i) {
+        dfi[i] = 0;
+        wfi[i] = 0;
+      });
+
+      elevators.forEach(function(elevator) {
+        flat.push(elevator.currentFloor);
+        flat.push(elevator.maxUsers);
+        flat.push(elevator.moveCount);
+        flat.push(elevator.velocityY);
+
+        elevator.userSlots.forEach(function(slot) {
+          if (slot.user !== null) {
+            dfi[slot.user.destinationFloor]++;
+            flat.push(slot.user.destinationFloor);
+            usersInElevators.push(slot.user);
+          }
+        });
+      });
+
+      world.users.forEach(function(user) {
+        if (usersInElevators.indexOf(user) < 0) {
+          wfi[user.destinationFloor]++;
+        }
+      });
+
+      return flat
+        .concat(dfi)
+        .concat(wfi);
+    },
+    think: function() {
+      this.brain.think();
+      return this;
+    }
+  };
+
+  return Elevia;
+})();
