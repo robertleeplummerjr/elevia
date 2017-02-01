dcdc2b4ce0acb6185d2032f9b5c1010192dcff79
Robert Plummer <robertleeplummerjr@gmail.com>
2017-02-01 14:18:45 -0500

    Update inputs.js

--- inputs.js
+++ inputs.js
@@ -1,6 +1,7 @@
-export default class {
-  constructor(inputs) {
-    this.inputs = inputs;
+export default class extends Array {
+  constructor() {
+    super();
+    this.add(Array.prototype.slice.call(arguments, 0));
   }
   
   reset() {
@@ -13,5 +14,13 @@ export default class {
     return this.inputs.map((input) => {
       return input.value;
     });
+  }
+  
+  add(input) {
+    if (typeof input === 'array') {
+      input.forEach((input) => this.add(input));
+      return;
+    }
+    this.push(input);
   }
 }
